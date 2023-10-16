000fa26c70ddc2b57a0a305368a339797493d339
jasmeen1234 <jasmeenbano12@gmail.com>
2023-10-17 01:47:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import CreateProject from './CreateProject';
+// import Register from './Register';
+import Login from './Login';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import ProjectList from './ProjectList';
+import Sidebar from './sidebar/Sidebar';
+import DashBoard from './DashBoard';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      
+       <Routes>
+       <Route path="/sidebar" element={<Sidebar/>} />
+       <Route path="creatproject" element={<CreateProject/>} />
+       {/* <Route path='/' element={<Register/>}/> */}
+       <Route path='/' element={<Login/>}/>
+       <Route path='/projectlist' element={<ProjectList/>}/>
+       <Route path='/dashboard' element={<DashBoard/>}/>
+       </Routes>
+      </BrowserRouter>
+     
+    </div>
+  );
+}
+
+export default App;
